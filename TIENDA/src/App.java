cdec73be68a4622897ff0aa5d55874795c9cdaa2
Natacha7 <jennynatacha@misena.edu.co>
2021-08-06 15:24:08 -0500

    Se crea clienteDAO y agrega JOptionPane

--- TIENDA/src/App.java
+++ TIENDA/src/App.java
@@ -1,13 +1,49 @@
+import modelo.*;
+
+import java.sql.ResultSet;
+
+import javax.swing.JOptionPane;
+
+import conexion.SqliteConnection;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("**La Tienda de Ropa**");
 
-        Producto productoUno = new Producto();
+    
 
-        productoUno.setNombre("Falda");
-        productoUno.setPrecio(70000);
-        productoUno.mostrar();
+       // Producto productoUno = new Producto();
 
-        
+        // productoUno.setNombre("Falda");
+        // productoUno.setPrecio(70000);
+        // productoUno.mostrar();
+
+        SqliteConnection conexionBD = new SqliteConnection();
+        ResultSet resultado = conexionBD.ejecutarConsulta("SELECT * FROM Productos");
+        while(resultado.next()) {
+            String codigoBarras = resultado.getString("CodigoBarras");
+            String nombreProducto = resultado.getString("nombre");
+            float precioProd = resultado.getFloat("precio");
+
+            System.out.println(codigoBarras + " " + nombreProducto + " $" + precioProd);
+        }
+
+        ///////////////-- Creación y consulta de clientes --/////////////////////
+
+        /*JOptionPane*/
+        String ident = JOptionPane.showInputDialog(null, "Ingrese la identificación del cliente", "LA TIENDA DE ROPA", JOptionPane.QUESTION_MESSAGE);
+        String nombre = JOptionPane.showInputDialog(null, "Ingrese el nombre del cliente", "LA TIENDA DE ROPA", JOptionPane.QUESTION_MESSAGE);
+        String apellido = JOptionPane.showInputDialog(null, "Ingrese el apellido del cliente", "LA TIENDA DE ROPA", JOptionPane.QUESTION_MESSAGE);
+
+        /*Objeto cliente*/
+        Cliente nuevoCliente = new Cliente(ident, nombre, apellido);
+
+        /*Objeto ClienteDAO para insertar cliente*/
+        ClienteDAO cli1 = new ClienteDAO();
+        cli1.insertarCliente(nuevoCliente, conexionBD);
+
+        /*Consulta cliente*/
+        String clientes = cli1.mostrarClientes(conexionBD);
+        JOptionPane.showMessageDialog(null, clientes, "Listado de clientes", JOptionPane.INFORMATION_MESSAGE);
     }
 }
